b349e6a2affa9f43d194218ff028e0711eb03755
Rustam <rustam.mustafoev01@gmail.com>
2023-06-19 15:04:46 +0500

    chore: add info to readme

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # Run the application as follows
 
+Make sure you have `Docker` and `docker-compose` installed on your machine. If you do have them, after cloning the
+repository change your directory to that cloned folder and run the following command on
+your terminal.
 ```bash
 docker-compose up -d --build
 ```
+The above command starts-up two containers. To check their state run the below command in your terminal.
+```bash
+docker-compose ps
+```
+Now, open any browser and type `http://0.0.0.0:8000/docs`. You should see the following image
+
+
+![API docs](./img/docs.png)
+
+
--- app/core/config.py
+++ app/core/config.py
@@ -5,11 +5,11 @@ DB_URL = "postgres://{postgres_user}:{postgres_password}@{postgres_host}:{postgr
 
 
 class PostgresSettings(BaseSettings):
-    postgres_user: str = Field(..., env="DB_USER")
-    postgres_password: str = Field(..., env="DB_PASSWORD")
-    postgres_db: str = Field(..., env="DB_NAME")
-    postgres_port: str = Field(..., env="DB_PORT")
-    postgres_host: str = Field(..., env="DB_HOST")
+    postgres_user: str = Field('impact', env="DB_USER")
+    postgres_password: str = Field('hub', env="DB_PASSWORD")
+    postgres_db: str = Field('booking', env="DB_NAME")
+    postgres_port: str = Field('5432', env="DB_PORT")
+    postgres_host: str = Field('db', env="DB_HOST")
 
 
 class TortoiseSettings(BaseSettings):
